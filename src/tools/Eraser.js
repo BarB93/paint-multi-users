7e745804b1c917dc629a23af124620ffd0f6b2e6
bbarovsky <barovskiyboris@gmail.com>
2022-03-14 03:31:59 +0300

    rewrited tool eraser use extends from brush

--- src/tools/Eraser.js
+++ src/tools/Eraser.js
@@ -1,39 +1,15 @@
-import Tool from "./Tool"
+import Brush from "./Brush"
 
-export default class Eraser extends Tool {
+export default class Eraser extends Brush {
 
     constructor(canvas) {
         super(canvas)
-        this.listen()
-    }
-
-    listen() {
-        this.canvas.onmouseup = this.mouseUpHandler.bind(this)
-        this.canvas.onmousedown = this.mouseDownHandler.bind(this)
-        this.canvas.onmousemove = this.mouseMoveHandler.bind(this)
-    }
-
-    mouseUpHandler(e) {
-        this.mouseDown = false
-    }
-
-    mouseDownHandler(e) {
-        this.mouseDown = true
-        this.ctx.beginPath()
-        this.ctx.moveTo(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop)
-    }
-
-    mouseMoveHandler(e) {
-        if (this.mouseDown) {
-            this.draw(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop)
-        }
     }
 
     draw(x, y) {
         this.ctx.lineTo(x, y)
         this.ctx.strokeStyle = '#FFFFFF'
         this.ctx.stroke()
-        console.log('draw brush')
     }
 
 }
